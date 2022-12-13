e71d759d55e6c7e9e2543eeb12fac1132a74bfe6
Rebecca Wollard <rbwollard@gmail.com>
2022-12-13 16:24:30 -0700

    commit tree 0c4b73a7525e036b2a2e754c6d653fba19339dea

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+let cats = ['Milo', `Otis`, 'Garfield']
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}   
+
+function  destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+
+
+function appendCat(name) {
+    const nameOne = [...cats, name]
+    return nameOne
+}
+
+function prependCat(name) {
+    const nameTwo = [name, ...cats]
+    return nameTwo
+}
+
+function removeLastCat() {
+    const kittens = cats.slice(0, cats.length - 1)
+    return kittens
+}
+
+function removeFirstCat() {
+    const lions = cats.slice(1)
+    return lions
+}
